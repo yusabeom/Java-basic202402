3c4fcbb824163ed01f541d76168c76d7d69ad42b
sabeom <tkqja717@naver.com>
2024-02-20 11:43:22 +0900

    # initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("안녕하세요 꾸렉꽥~!");
+        System.out.println("반갑습니다~!~!");
+    }
+}
