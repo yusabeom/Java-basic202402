17461ec0f6b34425292b30f980b316098e94c0b9
sabeom <tkqja717@naver.com>
2024-02-28 17:34:25 +0900

    # Java opp(exception기초)

---
+++ src/etc/exception/basic/TryCatchExample.java
@@ -0,0 +1,30 @@
+package etc.exception.basic;
+
+import java.util.Scanner;
+
+public class TryCatchExample {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+        System.out.println("정수1: ");
+        int i = sc.nextInt();
+        System.out.println("정수2: ");
+        int j = sc.nextInt();
+
+        System.out.println("나눗셈 시작!");
+
+            // try 블록에는 예외 발생 가능성이 있는 코드를 배치합니다.
+            System.out.printf("%d / %d = %d\n",i,j,i/j);
+        } catch (Exception e) {
+            // catch 블록에는 try블록에서 실제 예외가 발생했을 경우
+            // 실행할 코드를 작성합니다.
+            System.out.println("0으로 나누시면 어떡해요!");
+        }
+
+        System.out.println("프로그램 정상 종료!");
+
+        sc.close();
+    }
+}
