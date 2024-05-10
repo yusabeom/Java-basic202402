3003b429ce10af42dcda6fe8a3542a92d320f9e6
sabeom <tkqja717@naver.com>
2024-05-10 18:11:56 +0900

    # 240510 JAVA stream 연습문제

---
+++ src/etc/stream/quiz/Main.java
@@ -0,0 +1,91 @@
+package etc.stream.quiz;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = List.of(
+                new Transaction(brian, 2021, 300),
+                new Transaction(raoul, 2022, 1000),
+                new Transaction(raoul, 2021, 400),
+                new Transaction(mario, 2021, 710),
+                new Transaction(mario, 2022, 700),
+                new Transaction(alan, 2022, 950)
+        );
+
+        // 연습 1: 2021년에 발생한 모든 거래를 찾아 거래액 오름차 정렬(작은 값에서 큰 값).
+        transactions.stream()
+                .filter(t -> t.getYear() == 2021)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .forEach(System.out::println);
+
+
+        // 연습 2: 거래자가 근무하는 모든 도시 이름을 중복 없이 나열하시오.
+        System.out.println("2==================================");
+        transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .forEach(System.out::println);
+
+
+
+        // 연습 3: Cambridge에 근무하는 모든 거래자를 찾아 거래자 리스트로 이름순으로 오름차 정렬.
+        System.out.println("3==================================");
+        transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+
+        // 연습 4: 모든 거래자의 이름을 리스트에 모아서 알파벳순으로  오름차 정렬하여 반환
+        System.out.println("4==================================");
+        transactions.stream()
+                .map(Transaction::getTrader)
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+
+
+        // 연습 5: Milan에 거주하는 거래자가 한명이라도 있는지 여부 확인?
+        System.out.println("5==================================");
+        boolean milan = transactions.stream()
+                .map(Transaction::getTrader)
+                .anyMatch(trader -> trader.getCity().equals("Milan"));
+        System.out.println("milan = " + milan);
+
+
+        // 연습 6: Cambridge에 사는 거래자의 모든 거래액의 총합 출력.
+        System.out.println("6==================================");
+        transactions.stream()
+                .filter(trader -> trader.getTrader().getCity().equals("Cambridge"))
+                .map(Transaction::getValue);
+
+
+
+        // 연습 7: 모든 거래에서 최고 거래액은 얼마인가?
+        System.out.println("7==================================");
+        transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getValue).reversed())
+                .limit(1)
+                .forEach(System.out::println);
+
+
+        // 연습 8. 가장 작은 거래액을 가진 거래정보 탐색
+        System.out.println("8==================================");
+        transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .limit(1)
+                .forEach(System.out::println);
+    }
+}
